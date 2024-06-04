2d726505e187cd9dd2d40cf1e2fef0bf695ef21a
EnderWatts <endersoft@hotmail.com>
2024-06-03 23:49:28 -0500

    Initial Commit

---
+++ src/test/java/com/robertwmurphy/FinancialPlanner/FinancialPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertwmurphy.FinancialPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
